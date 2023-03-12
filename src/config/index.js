6d4ec21d2caa5e59df093b9286bf957212d56f71
silkster <dan@silkster.com>
2023-03-12 12:31:01 -0400

    update contact url & add recaptcha key to config

--- src/config/index.js
+++ src/config/index.js
@@ -3,8 +3,11 @@ import viewport from './viewport';
 export default {
   api: {
     contact: {
-      url: 'https://mxiltcmlm4.execute-api.us-east-1.amazonaws.com/default/sendContactEmail',
+      url: 'https://cxana18m0g.execute-api.us-east-2.amazonaws.com/default/sendEmailToSilkster',
     },
+  },
+  recaptcha: {
+    key: '6Lfsd_UkAAAAACqrQFNgYSoaIFSbDPW9TpmrGKDX',
   },
   viewport,
 };
